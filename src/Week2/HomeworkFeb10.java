179d1d5fabbef1d3e77c5cc2b81c84914580c4cb
KatHack11 <kkellie20@outlook.com>
2023-02-13 23:13:51 -0500

    The first Commit

---
+++ src/Week2/HomeworkFeb10.java
@@ -0,0 +1,14 @@
+package Week2;
+
+public class HomeworkFeb10 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Welcome to SDET course");
+
+        int length = 6;
+        int area;
+
+        int age = 37;
+        String name = "Katrina";
+    }
+}
---
+++ src/Week2/homeWork/CommentPractice.java
@@ -0,0 +1,43 @@
+package Week2.homeWork;
+//1-) Follow the below steps :
+//	a-) Create a class name called : MyHomeworkClass
+//	b-) Create a main metohd in MyHomeworkClass
+//	c-) Print the following text in CONSOLE
+//		"Java is a high-level, class-based, object-oriented programming language that is designed to have as few
+//		implementation dependencies as possible."
+/*
+1-) Follow the below steps :
+	a-) Create a class name called : MyHomeworkClass
+	b-) Create a main metohd in MyHomeworkClass
+	c-) Print the following text in CONSOLE
+		"Java is a high-level, class-based, object-oriented programming language that is designed to have as few
+		implementation dependencies as possible."
+ */
+/*
+2-) Write below sentences in comments
+	a-) This is single line comment
+	b-) This is regular multi-line comment
+	c-) This is Java Doc comment and it appears in green color.
+ */
+public class CommentPractice {
+    public static void main(String[] args) {
+        System.out.println("Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.");
+
+
+
+        //I'm just learning this
+        /*
+        This is a multi line
+        Comment for homework
+         */
+        /**
+         * This is an important note
+         * for review
+         * very important
+         */
+    }
+
+
+
+
+}
