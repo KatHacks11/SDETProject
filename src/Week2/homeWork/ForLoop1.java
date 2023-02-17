da8ce553a8782d46cf9430140c3fba2c89b86969
KatHacks11 <109197953+KatHacks11@users.noreply.github.com>
2023-02-16 23:05:52 -0500

    MultiplicationTable

---
+++ src/Week2/homeWork/ForLoop1.java
@@ -0,0 +1,16 @@
+package Week2.homeWork;
+
+public class ForLoop1 {
+    public static void  main(String[] args){
+        for(int i = 4; i>1; i--){
+            System.out.println("When i is less than or equal to 1, the code will continue from this line");
+
+            for (int i =1; i< 101; i++){
+                System.out.println(i + i++);
+
+            }
+
+        }
+    }
+
+}
---
+++ src/Week2/homeWork/MultiplicationTable.java
@@ -0,0 +1,7 @@
+package Week2.homeWork;
+
+public class MultiplicationTable {
+    public static void  main(String[] args){
+        //
+    }
+}
